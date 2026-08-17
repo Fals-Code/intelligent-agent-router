49042ddeff1884f0abc9c5a54be02c9f633c6cc7
Ahmad Mathlaul Falah <ahmadmathlaulfalah14@gmail.com>
2026-08-18 00:48:50 +0700

    Add durable workflow checkpoints and restart recovery

--- src/control-plane/index.ts
+++ src/control-plane/index.ts
@@ -2,6 +2,7 @@ export * from "./contracts.js";
 export * from "./capability-registry.js";
 export * from "./risk-policy.js";
 export * from "./workflow-state-machine.js";
+export * from "./durable-workflow.js";
 export * from "./run-ledger.js";
 export * from "./project-graph.js";
 export * from "./context-compiler.js";
